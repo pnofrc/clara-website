bc1b48fd02dca3d085562c6746d673d22a9337ec
poni <poni@riseup.net>
2021-02-23 12:36:18 +0100

    improv

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-window.onload = function(){
-    const video = document.querySelector("video");
-    const playBackSpeed = 100;
+// window.onload = function(){
+//     const video = document.querySelector("video");
+//     const playBackSpeed = 100;
 
-    document.body.style.height = Math.floor(
-        video.duration) * playBackSpeed + 'px';
+//     document.body.style.height = Math.floor(
+//         video.duration) * playBackSpeed + 'px';
 
-    window.addEventListener("scroll",function(){
-        video.currentTime = window.pageYOffset / (playBackSpeed+200);
-    });
+//     window.addEventListener("scroll",function(){
+//         video.currentTime = window.pageYOffset / (playBackSpeed+200);
+//     });
+// }
+
+
+var frameNumber = 0, // start video at frame 0
+    // lower numbers = faster playback
+    playbackConst = 500, 
+    // get page height from video duration
+    setHeight = document.getElementById("set-height"), 
+    // select video element         
+    vid = document.getElementById('v0'); 
+    // var vid = $('#v0')[0]; // jquery option
+
+// dynamically set the page height according to video length
+vid.addEventListener('loadedmetadata', function() {
+  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+});
+
+
+// Use requestAnimationFrame for smooth playback
+function scrollPlay(){  
+  var frameNumber  = window.pageYOffset/playbackConst;
+  vid.currentTime  = frameNumber;
+  window.requestAnimationFrame(scrollPlay);
 }
+
+window.requestAnimationFrame(scrollPlay);
